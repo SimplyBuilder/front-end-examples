4ed45e6bcd4ecab6d9f70ae46648e8fb8eb9acfe
JamilServices <jamilservicos@gmail.com>
2024-01-31 02:48:46 -0300

    Add vite.config.js file with Vite configuration.

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import path from 'node:path';
+export default defineConfig({
+    root: './src',
+    build: {
+        manifest: true,
+        outDir: '../dist',
+    },
+    css: {
+        postcss: {
+            config: './postcss.config.js'
+        }
+    },
+    publicDir: '../static',
+    resolve: {
+        alias: {
+            '@core-components': path.resolve(__dirname, 'src', 'core', 'components'),
+            '@core-stores': path.resolve(__dirname, 'src', 'core', 'stores'),
+            '@app-components': path.resolve(__dirname, 'src', 'app', 'components'),
+            '@app-stores': path.resolve(__dirname, 'src', 'app', 'stores'),
+            '@app-styles': path.resolve(__dirname, 'src', 'app', 'styles'),
+            '@styles': path.resolve(__dirname, 'src', 'styles'),
+        },
+    },
+    server: {
+        cors: false,
+        open: false
+    },
+    plugins: []
+});
