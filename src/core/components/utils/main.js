f777c4b2b7b801714af0e57e4e03f0267bd6ea16
JamilServices <jamilservicos@gmail.com>
2024-01-31 03:12:36 -0300

    Add UtilsApp module with CreateElementFromStruct and toRem functions.

---
+++ src/core/components/utils/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * Imports utility functions for converting pixel values to rem units and for creating elements from a structure.
+ */
+import {toRem} from "./toRem.js";
+import {CreateElementFromStruct} from "./createElement/main.js";
+
+
+/**
+ * @private
+ * @ignore
+ */
+const methods = {
+    CreateElementFromStruct,
+    toRem
+};
+Object.freeze(methods);
+
+/**
+ * @private
+ * @module UtilsApp
+ * @description This module acts as a utility hub, bundling various utility functions and making them available
+ * under the UtilsApp namespace. It is marked as private, indicating it's intended for internal use within the
+ * application or package.
+ *
+ * @property {Function} CreateElementFromStruct - A utility for creating DOM elements from a structured object.
+ * @property {Function} toRem - A utility for converting pixel values to rem units, aiding responsive design.
+ */
+export const UtilsApp = methods;
