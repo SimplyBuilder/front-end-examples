75c469170471c3b1137790615a4ee544d6f4dbf1
JamilServices <jamilservicos@gmail.com>
2024-01-31 04:01:20 -0300

    feat: Import core components in main.js
    Import core components UtilsApp, EventApp, DomStore, and EventStore in main.js. Add an event listener for the DOMContentLoaded event.
    
    Changes:
    - Added import statements for core components
    - Added event listener for DOMContentLoaded event

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 "use strict";
 
 import "@app-styles/main.scss";
+
+import {UtilsApp} from "@core-components/utils/main.js";
+import {EventApp} from "@core-components/eventApp/main.js";
+import {DomStore} from "@core-stores/domStore/main.js";
+import {EventStore} from "@core-stores/eventStore/main.js";
+
+const core = {UtilsApp, EventApp, DomStore, EventStore};
+Object.freeze(core);
+
+document.addEventListener("DOMContentLoaded",  () => {
+    if(UtilsApp && EventApp && DomStore && EventStore) {
+        console.log("DOMContentLoaded");
+    }
+});
